f0ab069e445e148281b0c2d1f8d9b95d15b31080
y.uchiida <y.uchiida@nirneege.com>
2021-09-20 02:18:04 +0900

    add files for contacts model hands-on

---
+++ app/Controller/ContactsController.php
@@ -0,0 +1,8 @@
+<?php
+
+App::uses('AppController', 'Controller');
+
+class ContactsController extends AppController {
+	/* スカフォールド機能を利用するため、$scafold を宣言 */
+	public $scaffold;
+}
---
+++ app/db_migration/create_contacts_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `cake2_training`.`contacts` (
+	`id` INT NOT NULL AUTO_INCREMENT ,
+	`nane` VARCHAR(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL ,
+	`gender` CHAR(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL ,
+	`email` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL ,
+	`contact_type` TINYINT NOT NULL ,
+	`content` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL ,
+	`confrim_privacy_policy` TINYINT NOT NULL , PRIMARY KEY  (`id`)
+) ENGINE = InnoDB;
