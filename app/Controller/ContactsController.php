ad5cb81fd624a93b2dc772b6b04d2536f470885a
y.uchiida <y.uchiida@nirneege.com>
2021-09-20 09:49:27 +0900

    add files for search records in tables via Contacts model hands-on

--- app/Controller/ContactsController.php
+++ app/Controller/ContactsController.php
@@ -4,5 +4,19 @@ App::uses('AppController', 'Controller');
 
 class ContactsController extends AppController {
 	/* スカフォールド機能を利用するため、$scafold を宣言 */
-	public $scaffold;
+	// public $scaffold;
+
+	public function index(){
+	
+		$this->autoLayout = false;
+
+		/* モデル経由でデータを取得する
+		 * Cake の命名規則に沿ったモデルとテーブルを自動で選択してSQLを実行するため、
+		 * 命名規則通りであれば、各コントローラファイル上でのモデルの読み込みは不要
+		 */
+		$contacts = $this->Contact->find('all');
+		$contacts_types = $this->Contact->findByContactType(1);
+		$this->set('contacts', $contacts);
+		$this->set('contacts_type', $contacts_types);
+	}
 }
---
+++ app/View/Contacts/index.ctp
@@ -0,0 +1,14 @@
+<pre>
+<?php
+/* ContactsController@index から受け取ったDBアクセスの一覧データを表示してみる */
+var_dump($contacts);
+?>
+</pre>
+
+<pre>
+
+<?php
+/* ContactsController@index から受け取ったDBアクセスの一覧データを表示してみる */
+var_dump($contacts_type);
+?>
+</pre>
