2a1b09a9884bd2f630156779d947ceb69d83a47f
yugo uchiida <y.uchiida@nirneege.com>
2021-10-02 18:17:32 +0900

    fix format and some typo

--- app/Controller/SampleController.php
+++ app/Controller/SampleController.php
@@ -6,21 +6,23 @@ App::uses('AppController', 'Controller');
  * Sample のアプリケーションのコントローラは「SampleController」になる
  * この名称にしておくことで、自動的にModelやView と関連付けされるらしい
  */
-class SampleController extends AppController{
+class SampleController extends AppController
+{
 
-	/* index アクションメソッド
-	 * URLは、 www.hostname/Sample/index/ になる
-	 * Laravelのように、ルーティング情報を設定しなくてもよい
-	 * index アクションはデフォルトなので、 www.hostname/Sample/でもよい(index 無しでOK)
-	 * index アクションがないと、Sample/ 直下へアクセスするとエラーになる
-	 */
-	public function index($param = "index"){
-		
-		/* viewを用いたレンダリングを無効にする */
-        $this -> autoRender = false;
+    /* index アクションメソッド
+     * URLは、 www.hostname/Sample/index/ になる
+     * Laravelのように、ルーティング情報を設定しなくてもよい
+     * index アクションはデフォルトなので、 www.hostname/Sample/でもよい(index 無しでOK)
+     * index アクションがないと、Sample/ 直下へアクセスするとエラーになる
+     */
+    public function index($param = "index")
+    {
 
-		/* 画面出力はprint/echo でできる */
-		print <<<_EOL
+        /* viewを用いたレンダリングを無効にする */
+        $this->autoRender = false;
+
+        /* 画面出力はprint/echo でできる */
+        print <<<_EOL
 <html>
 	<body>
 		<h1>/Sample/index</h1>
@@ -28,42 +30,45 @@ class SampleController extends AppController{
 		<p>\$param: $param</p>
 	</body>
 </html>
-_EOL ;
-	}
+_EOL;
+    }
 
-	public function other_action(){
-		
-		/* viewを用いたレンダリングを無効にする */
-        $this -> autoRender = false;
+    public function other_action()
+    {
 
-		/* 画面出力はprint/echo でできる */
-		print <<<_EOL
+        /* viewを用いたレンダリングを無効にする */
+        $this->autoRender = false;
+
+        /* 画面出力はprint/echo でできる */
+        print <<<_EOL
 <html>
 	<body>
 		<h1>/Sample/other_action</h1>
 		<p>this is SampleController@other_action</p>
 	</body>
 </html>
-_EOL ;
-	}
+_EOL;
+    }
 
-	/* 外部リダイレクト
-	 * $this->redirect('URL')
-	 * ちなみに、redirect() アクションメソッドはAppController で既に作成されている
-	 */
-	public function redirect_test(){
-		$this->redirect("https://google.co.jp");
-	}
+    /* 外部リダイレクト
+     * $this->redirect('URL')
+     * ちなみに、redirect() アクションメソッドはAppController で既に作成されている
+     */
+    public function redirect_test()
+    {
+        $this->redirect("https://google.co.jp");
+    }
 
-	/* 内部リダイレクト(フォワーディング)
-	 * $this->setAction('action_name', $param);
-	 * 同一コントローラ内の別のアクションメソッドへ転送できる
-	 * アクセス元のURLに変更はない
-	 * 第2引数でパラメータを設定可能
-	 * なお、別のコントローラのアクションメソッドに渡す場合は
-	 * App::import() が使えるらしい
-	 */
-	public function forwarding_test(){
-		$this->setAction("index", "forwarding_test");
-	}
+    /* 内部リダイレクト(フォワーディング)
+     * $this->setAction('action_name', $param);
+     * 同一コントローラ内の別のアクションメソッドへ転送できる
+     * アクセス元のURLに変更はない
+     * 第2引数でパラメータを設定可能
+     * なお、別のコントローラのアクションメソッドに渡す場合は
+     * App::import() が使えるらしい
+     */
+    public function forwarding_test()
+    {
+        $this->setAction("index", "forwarding_test");
+    }
 }
